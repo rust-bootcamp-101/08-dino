eff192ad34810cdcbc581ab789c9feab7030af18
startdusk <522383345@qq.com>
2024-08-03 09:35:07 +0800

    feature: default to IIFE for ModuleType

--- bundler/src/lib.rs
+++ bundler/src/lib.rs
@@ -23,7 +23,7 @@ mod tests {
         let ret = run_bundle("fixtures/main.ts", &Default::default())?;
         assert_eq!(
             ret,
-            r#"async function execute(name){console.log("Executing lib");return`Hello ${name}!`;}async function main(){console.log(`Executing main`);console.log(await execute("world"));}export{main as default};"#
+            r#"(function(){async function execute(name){console.log("Executing lib");return`Hello ${name}!`;}async function main(){console.log(`Executing main`);console.log(await execute("world"));}return{default:main};})();"#
         );
         Ok(())
     }
