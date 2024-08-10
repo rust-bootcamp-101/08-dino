3e85c3310ea72d91d1cac1c13637e7f708a662c6
startdusk <522383345@qq.com>
2024-08-10 18:51:30 +0800

    chore: improve code

--- dino/src/cli/run.rs
+++ dino/src/cli/run.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+use anyhow::Result;
 use clap::Parser;
 use dino_server::{start_server, ProjectConfig, SwappableAppRouter, TennetRouter};
 
@@ -13,17 +14,35 @@ pub struct RunOpts {
 }
 
 impl CmdExecutor for RunOpts {
-    async fn execute(self) -> anyhow::Result<()> {
+    async fn execute(self) -> Result<()> {
         let filename = build_project(".")?;
         let config = filename.replace(".mjs", ".yml");
         let code = fs::read_to_string(filename)?;
         let config = ProjectConfig::load(config)?;
-        let routers = vec![TennetRouter::new(
-            "localhost".to_string(),
-            SwappableAppRouter::try_new(&code, config.routes)?,
-        )];
+        let router = SwappableAppRouter::try_new(&code, config.routes)?;
+        let routers = vec![TennetRouter::new("localhost".to_string(), router.clone())];
+
+        // watch_project(".", router).await?;
+
         start_server(self.port, routers).await?;
 
         Ok(())
     }
 }
+
+// fn watch_project(dir: &'static str, router: SwappableAppRouter) -> Result<()> {
+//     let mut watcher = notify::recommended_watcher(move |res| match res {
+//         Ok(event) => {
+//             info!("event: {:?}", event);
+//             let filename = build_project(dir).unwrap();
+//             let config = filename.replace(".mjs", ".yml");
+//             let code = fs::read_to_string(filename).unwrap();
+//             let config = ProjectConfig::load(config).unwrap();
+//             router.swap(code, config.routes).unwrap();
+//         }
+//         Err(e) => println!("watch error: {:?}", e),
+//     })?;
+
+//     watcher.watch(Path::new(dir), RecursiveMode::Recursive)?;
+//     Ok(())
+// }
